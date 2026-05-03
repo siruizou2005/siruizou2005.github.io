18a3e75c4a7a2d6ef03cbe6c6bab7c4cf5067fee
siruizou2005 <siruizou2005@gmail.com>
2026-05-02 21:33:15 -0700

    Optimize performance and improve Research page layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,29 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
-import Research from './pages/Research';
-import CV from './pages/CV';
-import Blog from './pages/Blog';
-import BlogPost from './pages/BlogPost';
-import Gallery from './pages/Gallery';
+
+const Research = lazy(() => import('./pages/Research'));
+const CV = lazy(() => import('./pages/CV'));
+const Blog = lazy(() => import('./pages/Blog'));
+const BlogPost = lazy(() => import('./pages/BlogPost'));
+const Gallery = lazy(() => import('./pages/Gallery'));
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="research" element={<Research />} />
-          <Route path="cv" element={<CV />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="blog/:id" element={<BlogPost />} />
-          <Route path="gallery" element={<Gallery />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="research" element={<Research />} />
+            <Route path="cv" element={<CV />} />
+            <Route path="blog" element={<Blog />} />
+            <Route path="blog/:id" element={<BlogPost />} />
+            <Route path="gallery" element={<Gallery />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
